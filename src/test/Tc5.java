7f22cb0a0c2f0ea0cf144d32f65e9c34bf38c893
fatimaknemesis <fatimaknemesis@gmail.com>
2022-01-09 14:54:52 -0600

    Update Tc5.java

--- src/test/Tc5.java
+++ src/test/Tc5.java
@@ -4,7 +4,7 @@ public class Tc5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi there");
+System.out.println("hi there beautiful");
 	}
 
 }
